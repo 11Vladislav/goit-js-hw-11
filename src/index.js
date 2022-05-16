5278517f158b88042ccd13c2d9ed741d6b2dbfd4
Vladislav Ponomarev <vlad11ponomarev@gmail.com>
2022-05-16 08:03:07 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './sass/main.scss';
+import throttle from 'lodash.throttle';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
+import photoCardsMarkUp from './templates/photo-cards.hbs';
